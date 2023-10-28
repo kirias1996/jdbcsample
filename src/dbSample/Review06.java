be8403f32e7242343afbdba99deb9db47c236652
shout <shout@192.168.11.4>
2023-10-28 11:16:11 +0900

    オブジェクト指向を用いたDB連携のコード実装

---
+++ src/dbSample/Review06.java
@@ -0,0 +1,94 @@
+package dbSample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Review06 {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		PreparedStatement pstmt=  null;
+		ResultSet rs =null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+					"root",
+					"Arishou1996!");
+
+			String sql = "SELECT * FROM person WHERE id = ?";
+			pstmt = con.prepareStatement(sql);
+
+//			キーワード入力させる
+			System.out.print("検索キーワードを入力してください。>");
+			int id = Integer.parseInt(keyIn());
+
+			pstmt.setInt(1, id);
+			rs = pstmt.executeQuery();
+
+			while( rs.next() ){
+
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+
+                System.out.println(name);
+                System.out.println(age);
+
+            }
+
+		} catch (ClassNotFoundException e) {
+			System.err.println("JDBCドライバーのロードに失敗しました。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("データベースに異常が発生しました。");
+			e.printStackTrace();
+		}finally {
+			if( rs != null ){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if( pstmt != null ){
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    System.err.println("Statementを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+			if (con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					System.err.println("データベース切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	private static String keyIn() {
+		String line = null;
+		try {
+			BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+			line = key.readLine();
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+		return line;
+	}
+}
+
+
